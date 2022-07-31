1f4684b943037e86a86c5fba364a14858d80362b
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-07-31 21:25:59 +0430

    Update Contact And Impliment to IContact

--- Contact.cs
+++ Contact.cs
@@ -4,38 +4,38 @@ using System.Text;
 
 namespace TelephoneNotebook
 {
-    public class Contact
+    public class Contact : IContact
     {
         #region Attributes
 
-        private int _fullName;
-        private int _phoneNumber;
-        private int _emailAddress;
-        private int _homeAddress;
+        private string _fullName;
+        private long _phoneNumber;
+        private string _emailAddress;
+        private string _homeAddress;
 
         #endregion
 
 
         #region Set & Get
 
-        public int FullName
+        public string FullName
         {
             get { return _fullName; }
             set { _fullName = value; }
         }
 
-        public int PhoneNumber
+        public long PhoneNumber
         {
             get { return _phoneNumber; }
             set { _phoneNumber = value; }
         }
 
-        public int EmailAddress
+        public string EmailAddress
         {
             get { return _emailAddress; }
             set { _emailAddress = value; }
         }
-        public int HomeAddress
+        public string HomeAddress
         {
             get { return _homeAddress; }
             set { _homeAddress = value; }
