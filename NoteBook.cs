afd0ea171a51d10826789cad337ef945c40dde03
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-07-31 21:24:22 +0430

    Add Note Book class

---
+++ NoteBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelephoneNotebook
+{
+    public class NoteBook
+    {
+        #region Atributes 
+
+        private List<Contact> _contactList = new List<Contact>();
+
+        #endregion
+
+
+        #region Oprations
+
+       
+
+
+        #endregion
+
+
+    }
+}
