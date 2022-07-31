004bbced6dfbcafb1c1dd9ad7c32debfab95aa73
SinaJB <sinajabermoradi313@gmail.com>
2022-07-31 16:49:46 +0430

    Creat Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelephoneNotebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
