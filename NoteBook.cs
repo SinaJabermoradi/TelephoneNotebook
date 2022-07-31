ab7e5412f46d54b582e7c935dc7cdffc2338a6a9
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-07-31 22:37:48 +0430

    Update And Finish NoteBook Business

--- NoteBook.cs
+++ NoteBook.cs
@@ -4,19 +4,103 @@ using System.Text;
 
 namespace TelephoneNotebook
 {
-    public class NoteBook
+    public class NoteBook : INoteBook
     {
         #region Atributes 
 
-        private List<Contact> _contactList = new List<Contact>();
+        private List<Contact> _contactList = null;
+        private int _personCount = 0;
 
         #endregion
 
+        #region Constractors
+
+        public NoteBook()
+        {
+            _contactList = new List<Contact>();
+        }
+
+        #endregion
+
+        #region Setter & Getter
+
+        public int TotalContacts
+        {
+            get
+            {
+                for (int temp = 0; temp < _contactList.Count; temp++)
+                {
+                    if (_contactList[temp] != null)
+                        _personCount++;
+                }
+                return _personCount;
+            }
+        }
+
+        #endregion
 
         #region Oprations
 
-       
+        public void Add(IContact newPerson)
+        {
+            foreach (Contact person in _contactList)
+            {
+                person.FullName = newPerson.FullName;
+                person.PhoneNumber = newPerson.PhoneNumber;
+                person.EmailAddress = person.EmailAddress;
+                person.HomeAddress = newPerson.HomeAddress;
+            }
+        }
 
+        public void Delete(string userPhoneNumber)
+        {
+            for (int temp = 0; temp < _contactList.Count; temp++)
+            {
+                if (_contactList[temp] != null && _contactList[temp].PhoneNumber == Convert.ToInt64(userPhoneNumber))
+                {
+                    _contactList[temp] = null;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("This Number does Not Exist");
+                }
+            }
+        }
+
+        public IContact SearchByName(string userName)
+        {
+            for (int temp = 0; temp < _contactList.Count; temp++)
+            {
+                if (_contactList[temp] != null && _contactList[temp].FullName == userName)
+                    return _contactList[temp];
+            }
+            return null;
+        }
+
+        public IContact SearchByNumber(string userPhoneNumber)
+        {
+            for (int temp = 0; temp < _contactList.Count; temp++)
+            {
+                if (_contactList[temp] != null && _contactList[temp].PhoneNumber == Convert.ToInt64(userPhoneNumber))
+                    return _contactList[temp];
+            }
+            return null;
+        }
+
+        public void Update(string userPhoneNumber, Contact newPerson)
+        {
+            for (int temp = 0; temp < _contactList.Count; temp++)
+            {
+                if (_contactList[temp].PhoneNumber == Convert.ToInt64(userPhoneNumber))
+                {
+                    _contactList[temp].FullName = newPerson.FullName;
+                    _contactList[temp].PhoneNumber = newPerson.PhoneNumber;
+                    _contactList[temp].EmailAddress = newPerson.EmailAddress;
+                    _contactList[temp].HomeAddress = newPerson.HomeAddress;
+                }
+            }
+        }
 
         #endregion
 
--- Program.cs
+++ Program.cs
@@ -2,28 +2,93 @@
 
 namespace TelephoneNotebook
 {
-    class Program
+    public class Program
     {
+
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("Wellcome To Telphone Notebook");
-            Console.WriteLine("--------------------------------------------");
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("1.Add");
-            Console.WriteLine("2.Update");
-            Console.WriteLine("3.Delete"); 
-            Console.WriteLine("4.Search By Name"); 
-            Console.WriteLine("5.Search By Number"); 
-            Console.WriteLine("6.Exit");
-            Console.WriteLine();
-            Console.WriteLine("Please Select an options (1 to 6): ");
+            INoteBook PhoneBook = new NoteBook();
 
-            string option = Console.ReadLine();
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Wellcome To Telphone Notebook");
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("A => Add");
+                Console.WriteLine("U => Update");
+                Console.WriteLine("D => Delete");
+                Console.WriteLine("SNA => Search By Name");
+                Console.WriteLine("SNU => Search By Number");
+                Console.WriteLine("E => Exit");
+                Console.WriteLine();
+                Console.Write("Please Select an options (1 to 6): ");
+
+                string option = Console.ReadLine();
 
 
-            Console.ReadKey();
+                switch (option.ToLower())
+                {
+                    case "a":
+                        {
+                            IContact contact = new Contact();
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                           
+                            Console.WriteLine("--------------------------------------------------------");
+                            Console.Write("FullName : ");
+                            contact.FullName = Console.ReadLine();
+
+                            Console.Write("PhoneNumber : ");
+                            contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+
+                            Console.Write("EmailAddress : ");
+                            contact.EmailAddress = Console.ReadLine();
+
+                            Console.Write("HomeAddress : ");
+                            contact.HomeAddress = Console.ReadLine();
+
+                            PhoneBook.Add(contact);
+
+                            break;
+                        }
+                    case "u":
+                        {
+
+                            break;
+                        }
+                    case "d":
+                        {
+
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            Console.WriteLine("--------------------------------------------------------");
+                            Console.Write("UserPhoneNumber : ");
+                            string UserNumber = Console.ReadLine();
+                            PhoneBook.Delete(UserNumber);
+                            
+                            break;
+                        }
+                    case "4":
+                        {
+
+                            break;
+                        }
+                    case "5":
+                        {
+
+                            break;
+                        }
+                    case "6":
+                        {
+
+                            break;
+                        }
+                }
+
+                Console.ReadKey();
+
+            }
         }
     }
 }
