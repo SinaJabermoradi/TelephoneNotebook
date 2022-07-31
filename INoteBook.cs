81dc9f439337aacde82f9474f2f88a5a6d8cb5f2
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-07-31 22:36:50 +0430

    Edit And Update (( Contact )) To (( IContact ))

--- INoteBook.cs
+++ INoteBook.cs
@@ -6,11 +6,11 @@ namespace TelephoneNotebook
 {
     public interface INoteBook
     {
-        public void Add(Contact person);
+        public void Add(IContact person);
         public void Delete(string userPhoneNumber);
         public int TotalContacts { get; }
-        public Contact SearchByName(string userName);
-        public Contact SearchByNumber(string userPhoneNumber);
+        public IContact SearchByName(string userName);
+        public IContact SearchByNumber(string userPhoneNumber);
         public void Update(string userPhoneNumber,Contact person);
 
     }
