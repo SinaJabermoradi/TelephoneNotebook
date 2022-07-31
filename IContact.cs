644568b8e3f95bbe1ef986b19cdb5bd9727fb883
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-07-31 21:24:00 +0430

    add Interfaces (( IContact )) & (( INoteBook )) For Rules Projects

---
+++ IContact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelephoneNotebook
+{
+    public interface IContact
+    {
+        public string FullName { get; set; } 
+        public long PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string HomeAddress { get; set; }
+
+    }
+}
---
+++ INoteBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelephoneNotebook
+{
+    public interface INoteBook
+    {
+        public void Add(Contact person);
+        public void Delete(string userPhoneNumber);
+        public int TotalContacts { get; }
+        public Contact SearchByName(string userName);
+        public Contact SearchByNumber(string userPhoneNumber);
+        public void Update(string userPhoneNumber,Contact person);
+
+    }
+}
