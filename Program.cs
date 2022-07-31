84d3eb4a51531fc374dde6b9fd4f742290ee834c
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-07-31 17:12:23 +0430

    Add Menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace TelephoneNotebook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Wellcome To Telphone Notebook");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("1.Add");
+            Console.WriteLine("2.Update");
+            Console.WriteLine("3.Delete"); 
+            Console.WriteLine("4.Search By Name"); 
+            Console.WriteLine("5.Search By Number"); 
+            Console.WriteLine("6.Exit");
+            Console.WriteLine();
+            Console.WriteLine("Please Select an options (1 to 6): ");
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
